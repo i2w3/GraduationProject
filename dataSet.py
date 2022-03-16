0f497b7705de6db80be4b16479dc1cdbe26820fc
iwhoi <1057887863@qq.com>
2022-03-16 13:00:02 +0800

    Add files via upload

---
+++ dataSet.py
@@ -0,0 +1,44 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+def trans_Compose(resize=None, normalize=None):
+    trans = [transforms.ToTensor()]  # 转换data的数据类型为Torch Tensor
+    if resize:
+        trans.insert(0, transforms.Resize(resize))  # 调整data的size
+    if normalize:
+        trans.append(transforms.Normalize(normalize, normalize))  # 需不需要标准化(0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+    return transforms.Compose(trans)
+
+
+# 加载数据集
+def load_MNIST(batch_size, resize=None, normalize=None):
+    MNIST_train = datasets.MNIST(root=r'.\data',  # 数据保存路径
+                                 train=True,  # 作为训练集
+                                 download=True,  # 是否下载该数据集
+                                 transform=trans_Compose(resize, normalize)
+                                 )
+    MNIST_test = datasets.MNIST(root=r'.\data',  # 数据保存路径
+                                train=False,  # 作为测试集
+                                download=True,  # 是否下载该数据集
+                                transform=trans_Compose(resize, normalize)
+                                )
+    return (DataLoader(MNIST_train, batch_size=batch_size, shuffle=True),
+            DataLoader(MNIST_test, batch_size=batch_size, shuffle=False),
+            1)
+
+
+def load_CIFAR10(batch_size, resize=None, normalize=None):
+    CIFAR10_train = datasets.CIFAR10(root=r'.\data',  # 数据保存路径
+                                     train=True,  # 作为训练集
+                                     download=True,  # 是否下载该数据集
+                                     transform=trans_Compose(resize, normalize)
+                                     )
+    CIFAR10_test = datasets.CIFAR10(root=r'.\data',  # 数据保存路径
+                                    train=False,  # 作为测试集
+                                    download=True,  # 是否下载该数据集
+                                    transform=trans_Compose(resize, normalize)
+                                    )
+    return (DataLoader(CIFAR10_train, batch_size=batch_size, shuffle=True),
+            DataLoader(CIFAR10_test, batch_size=batch_size, shuffle=False),
+            3)
