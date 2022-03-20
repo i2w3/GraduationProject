a8336f8441e2ffa19af8af6d2ae8250f6a8292bd
Nya2233 <1057887863@qq.com>
2022-03-20 16:46:27 +0800

    修复长行代码在colab上的错误

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ N_EPOCHS = 50
 # train_loader, valid_loader, channel = load_MNIST(BATCH_SIZE, resize=(32, 32))
 train_loader, valid_loader, channel = load_CIFAR10(BATCH_SIZE, resize=(32, 32), normalize=True, Random=True)
 
-
 net = Lenet(channel)
 # net = Lenet(channel, BN2=True)
 # net = Lenet(channel, SE2=True, BN2=True, reduction=16)
@@ -35,5 +34,5 @@ print('#Params: %.1fM' % params)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 criterion = nn.CrossEntropyLoss()
 model, optimizer, (train_loss, valid_loss), (train_acc, valid_acc) = training_loop(model, criterion, optimizer,
-                                                                                   train_loader, valid_loader,
-                                                                                   N_EPOCHS, DEVICE)
+                                                                                   train_loader, valid_loader, N_EPOCHS,
+                                                                                   DEVICE)
