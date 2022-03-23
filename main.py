ebd846803664105187e89de86548f1100ff0929c
Nya2233 <1057887863@qq.com>
2022-03-23 18:47:40 +0800

    fix

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ EPOCHS = 130
 # train_loader, valid_loader, channel = load_MNIST(BATCH_SIZE, resize=(32, 32))
 train_loader, valid_loader, channel = load_CIFAR10(BATCH_SIZE, normalize=True, Random=True)
 
-# net = resnet18()
-net = se_resnet18()
+net = resnet18()
+# net = se_resnet18()
+
 # net = Lenet(channel)
 # net = Lenet(channel, BN2=True)
 # net = Lenet(channel, SE2=True, BN2=True, reduction=16)
@@ -29,13 +30,13 @@ net = se_resnet18()
 # net = Lenet(channel, SE1=True, BN1=True, SE2=True, BN2=True)
 # net = Lenet(channel, BN1=True, BN2=True)
 
-
+model = net.to(device)
 # summary(model, (channel, 32, 32))
-summary(net, (channel, 224, 224))
-params = sum(p.numel() for p in list(net.parameters())) / 1e6
+summary(model, (channel, 224, 224))
+params = sum(p.numel() for p in list(model.parameters())) / 1e6
 print('#Params: %.1fM' % params)
 
 # optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 optimizer = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=5e-4)
 criterion = nn.CrossEntropyLoss()
-net, optimizer, (_, _), (_, _) = training_loop(net, criterion, optimizer, train_loader, valid_loader, device,EPOCHS)
+model, optimizer, (_, _), (_, _) = training_loop(model, criterion, optimizer, train_loader, valid_loader, device,EPOCHS)
