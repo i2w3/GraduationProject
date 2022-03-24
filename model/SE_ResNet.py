8042bcc87484ec964b8c67510d10726095f3b327
Nya2233 <1057887863@qq.com>
2022-03-24 15:29:46 +0800

    fix

--- model/SE_ResNet.py
+++ model/SE_ResNet.py
@@ -5,6 +5,7 @@ from torchvision.models import ResNet
 构建se_resnet18
 '''
 
+
 # 3x3卷积层
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
