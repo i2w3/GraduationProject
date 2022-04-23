32845b579cef4b00b76622fc06664a46ef8c99ed
iwhoi <1057887863@qq.com>
2022-04-23 12:50:39 +0800

    update

--- dataSet.py
+++ dataSet.py
@@ -1,126 +1,64 @@
 import torch
-import random
-import numpy as np
-from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 
-def trans_Compose(Resize=None, Normalize=None, Random=None, Noise=None):
-    trans = [transforms.ToTensor()]
-    if Random:
-        trans.insert(0, transforms.RandomCrop(32, padding=4))  # 数据增广
-        trans.insert(0, transforms.RandomHorizontalFlip())  # 依50%概率水平翻转
-        print(f"Dataset enable Random")
-    if Resize:
-        trans.insert(0, transforms.Resize(Resize))  # 调整data的size
-    if Normalize:
-        if Normalize == "MNIST":
-            trans.append(transforms.Normalize([0.1307, ], [0.3081, ]))
-        if Normalize == "CIFAR10":
-            trans.append(transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]))
-        if Normalize == "CIFAR100":
-            trans.append(transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762]))
-        print(f"Dataset enable Normalize")
-    if Noise:
-        trans.append(transforms.RandomApply([AddGaussianNoise(0., 1.)], p=0.3))
-        print(f"Dataset enable Noise")
-    return transforms.Compose(trans)
+def transform_Augment(Augment):
+    if Augment == 'CIFAR10':
+        transform = transforms.Compose([transforms.RandomCrop(32, padding=4),  # 先四周填充4，在把图像随机裁剪成32*32
+                                        transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+                                        transforms.ToTensor(),
+                                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                                        # R,G,B每层的归一化用到的均值和方差
+                                        ])
+    elif Augment == 'MNIST':
+        transform = transforms.Compose([transforms.RandomCrop(32, padding=4),  # 先四周填充4，在把图像随机裁剪成32*32
+                                        transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.1307, ], [0.3081, ])
+                                        # R,G,B每层的归一化用到的均值和方差
+                                        ])
+    return transform
 
 
-# 加载数据集
-def load_MNIST(batch_size, Resize=None, Normalize=None, Random=None, Noise=None):
-    if Normalize:
-        normalize = "MNIST"
+def download_CIFAR10(DownloadRoot, Augment):
+    transform_test = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                                         ])
+
+    if Augment:
+        transform_train = transform_Augment('CIFAR10')
     else:
-        normalize = None
-    MNIST_train = datasets.MNIST(root=r'.\data',  # 数据保存路径
-                                 train=True,  # 作为训练集
-                                 download=True,  # 是否下载该数据集
-                                 transform=trans_Compose(Resize, normalize, Random, Noise)
-                                 )
-    print(f"MNIST训练数据集处理完成")
-    MNIST_test = datasets.MNIST(root=r'.\data',  # 数据保存路径
-                                train=False,  # 作为测试集
-                                download=True,  # 是否下载该数据集
-                                transform=trans_Compose(Resize, normalize)
-                                )
-    print(f"MNIST测试数据集处理完成")
-    return (DataLoader(MNIST_train, batch_size=batch_size, shuffle=True),
-            DataLoader(MNIST_test, batch_size=batch_size, shuffle=False),
-            1)
+        transform_train = transform_test
 
-
-def load_CIFAR10(batch_size, Resize=None, Normalize=None, Random=None, Noise=None, Shuffle=False):
-    if Normalize:
-        normalize = "CIFAR10"
-    else:
-        normalize = None
-    CIFAR10_train = datasets.CIFAR10(root=r'.\data',  # 数据保存路径
+    CIFAR10_train = datasets.CIFAR10(root=DownloadRoot,  # 数据保存路径
                                      train=True,  # 作为训练集
                                      download=True,  # 是否下载该数据集
-                                     transform=trans_Compose(Normalize=normalize, Random=Random, Noise=Noise)
+                                     transform=transform_train
                                      )
-    print(f"CIFAR10训练数据集处理完成")
-    CIFAR10_test = datasets.CIFAR10(root=r'.\data',  # 数据保存路径
+
+    CIFAR10_test = datasets.CIFAR10(root=DownloadRoot,  # 数据保存路径
                                     train=False,  # 作为测试集
                                     download=True,  # 是否下载该数据集
-                                    transform=trans_Compose(Normalize=normalize)
+                                    transform=transform_test
                                     )
-    print(f"CIFAR10测试数据集处理完成")
-    return (DataLoader(CIFAR10_train, batch_size=batch_size, shuffle=True),
-            DataLoader(CIFAR10_test, batch_size=batch_size, shuffle=Shuffle),
-            3)
+    return CIFAR10_train, CIFAR10_test
 
 
-def load_CIFAR100(batch_size, Resize=None, Normalize=None, Random=None, Noise=None, Shuffle=False):
-    if Normalize:
-        normalize = "CIFAR100"
+def CIFAR10_Dataloader(batch_size, Augment=True, DownloadRoot=r".\dataSet"):
+    CIFAR10_train, CIFAR10_test = download_CIFAR10(DownloadRoot, Augment)
+
+    if batch_size == 'BGD':
+        train_batch_size = CIFAR10_train.data.shape[0]  # 50000
+        test_batch_size = CIFAR10_test.data.shape[0]  # 10000
+    elif batch_size == 'SGD':
+        train_batch_size = 1
+        test_batch_size = 1
     else:
-        normalize = None
-    CIFAR100_train = datasets.CIFAR100(root=r'.\data',  # 数据保存路径
-                                       train=True,  # 作为训练集
-                                       download=True,  # 是否下载该数据集
-                                       transform=trans_Compose(Normalize=normalize, Random=Random, Noise=Noise)
-                                       )
-    print(f"CIFAR100训练数据集处理完成")
-    CIFAR100_test = datasets.CIFAR100(root=r'.\data',  # 数据保存路径
-                                      train=False,  # 作为测试集
-                                      download=True,  # 是否下载该数据集
-                                      transform=trans_Compose(Normalize=normalize)
-                                      )
-    print(f"CIFAR100测试数据集处理完成")
-    return (DataLoader(CIFAR100_train, batch_size=batch_size, shuffle=True),
-            DataLoader(CIFAR100_test, batch_size=batch_size, shuffle=Shuffle),
-            3)
+        train_batch_size = batch_size
+        test_batch_size = batch_size
 
+    train_loader = DataLoader(CIFAR10_train, batch_size=train_batch_size, shuffle=False)
+    test_loader = DataLoader(CIFAR10_test, batch_size=test_batch_size, shuffle=False)
 
-class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=1.):
-        self.std = std
-        self.mean = mean
-
-    def __call__(self, tensor):
-        return tensor + torch.randn(tensor.size()) * self.std + self.mean
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
-
-
-class AddSaltPepperNoise(object):
-
-    def __init__(self, density=0):
-        self.density = density
-
-    def __call__(self, img):
-
-        img = np.array(img)                                                             # 图片转numpy
-        h, w, c = img.shape
-        Nd = self.density
-        Sd = 1 - Nd
-        mask = np.random.choice((0, 1, 2), size=(h, w, 1), p=[Nd/2.0, Nd/2.0, Sd])      # 生成一个通道的mask
-        mask = np.repeat(mask, c, axis=2)                                               # 在通道的维度复制，生成彩色的mask
-        img[mask == 0] = 0                                                              # 椒
-        img[mask == 1] = 255                                                            # 盐
-        img= Image.fromarray(img.astype('uint8')).convert('RGB')                        # numpy转图片
-        return img
+    return train_loader, test_loader, 3
---
+++ dataSet/duel_Plot.py
@@ -0,0 +1,142 @@
+from utils import *
+
+
+def load_Npy(npy_path):
+    train_acc = np.load(npy_path + "train_acc.npy", encoding="latin1")
+    valid_acc = np.load(npy_path + "valid_acc.npy", encoding="latin1")
+
+    train_loss = np.load(npy_path + "train_loss.npy", encoding="latin1")
+    valid_loss = np.load(npy_path + "valid_loss.npy", encoding="latin1")
+
+    return (train_acc, valid_acc), (train_loss, valid_loss)
+
+
+def full_Plot():
+    size = 15
+
+    (train_acc1, valid_acc1), (train_loss1, valid_loss1) = load_Npy(r"./无数据增强CIFAR10/1650641318/")
+
+    train_loss1 = np.array(train_loss1)
+    valid_loss1 = np.array(valid_loss1)
+    train_acc1 = np.array(train_acc1)
+    valid_acc1 = np.array(valid_acc1)
+
+    (train_acc2, valid_acc2), (train_loss2, valid_loss2) = load_Npy(r"./无数据增强CIFAR10/1650643962/")
+
+    train_loss2 = np.array(train_loss2)
+    valid_loss2 = np.array(valid_loss2)
+    train_acc2 = np.array(train_acc2)
+    valid_acc2 = np.array(valid_acc2)
+
+    loss1_max = np.max(train_loss1) if np.max(train_loss1) > np.max(valid_loss1) else np.max(valid_loss1)
+    loss2_max = np.max(train_loss2) if np.max(train_loss2) > np.max(valid_loss2) else np.max(valid_loss2)
+    loss_max = loss1_max if loss1_max > loss2_max else loss2_max
+
+    acc1_min = np.min(train_acc1) if np.min(train_acc1) < np.min(valid_acc1) else np.min(valid_acc1)
+    acc2_min = np.min(train_acc2) if np.min(train_acc2) < np.min(valid_acc2) else np.min(valid_acc2)
+    acc_min = acc1_min if acc1_min < acc2_min else acc2_min
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(14, 5), tight_layout=True)  # 创建一个包含一个axes的figure
+
+    ax[0].set(ylim=(0.00, loss_max))
+    ax[1].set(ylim=(0.00, loss_max))
+
+    l1 = ax[0].plot(train_loss1, '--', color='blue', label="Train loss")
+    l2 = ax[0].plot(valid_loss1, '--', color='red', label="Valid loss")
+    ax2 = ax[0].twinx()
+    ax2.set(ylim=(acc_min, 1.00))
+
+    l3 = ax2.plot(train_acc1, color='blue', label="Train acc")
+    l4 = ax2.plot(valid_acc1, color='red', label="Valid acc")
+
+    lns = l1 + l2 + l3 + l4
+    labs = [l.get_label() for l in lns]
+    ax[0].legend(lns, labs, loc='upper left')
+    # plt.tick_params(labelsize=size)
+    EPOCHS = np.array(valid_acc1).size
+    if EPOCHS == 15:
+        blink = 2
+    elif EPOCHS == 50:
+        blink = 7
+    elif EPOCHS == 100:
+        blink = 11
+    elif EPOCHS == 240:
+        blink = 34
+    else:
+        blink = 18
+    ticks = list(range(0, EPOCHS, blink))
+    if EPOCHS == 15 or EPOCHS == 50 or EPOCHS == 100 or EPOCHS == 200:
+        pass
+    else:
+        ticks[-1] = EPOCHS
+
+    tickl = [i + 1 for i in ticks]
+    ax[0].set_xticks(ticks)
+    ax[0].set_xticklabels(tickl, rotation=10)
+
+    # ax[0].set_title("Acc & Loss over Epochs", fontsize=size)
+    ax[0].set_xlabel('Epoch', fontsize=size)
+    ax[0].set_ylabel("Loss", fontsize=size)
+
+    # ax2.set_ylabel("Acc", fontsize=size)
+
+    ax[0].grid(b=False, axis="y")
+    ax2.grid(b=False, axis="y")
+    ax[0].grid(b=True, axis="x")
+
+    # subplot(2)
+
+    l1 = ax[1].plot(train_loss2, '--', color='blue', label="Train loss")
+    l2 = ax[1].plot(valid_loss2, '--', color='red', label="Valid loss")
+    # ax.set_ylable(labelsize=size)
+    # plt.tick_params(labelsize=size)
+    ax2 = ax[1].twinx()
+    ax2.set(ylim=(acc_min, 1.00))
+
+    l3 = ax2.plot(train_acc2, color='blue', label="Train acc")
+    l4 = ax2.plot(valid_acc2, color='red', label="Valid acc")
+
+    lns = l1 + l2 + l3 + l4
+    labs = [l.get_label() for l in lns]
+    ax[1].legend(lns, labs, loc='upper left')
+    # plt.tick_params(labelsize=size)
+    EPOCHS = np.array(valid_acc2).size
+    if EPOCHS == 15:
+        blink = 2
+    elif EPOCHS == 50:
+        blink = 7
+    elif EPOCHS == 100:
+        blink = 11
+    elif EPOCHS == 240:
+        blink = 34
+    else:
+        blink = 18
+    ticks = list(range(0, EPOCHS, blink))
+    if EPOCHS == 15 or EPOCHS == 50 or EPOCHS == 100:
+        pass
+    else:
+        ticks[-1] = EPOCHS - 1
+
+    print(ticks)
+
+    tickl = [i + 1 for i in ticks]
+    ax[1].set_xticks(ticks)
+    ax[1].set_xticklabels(tickl, rotation=10)
+
+    # ax[0].set_title("Acc & Loss over Epochs", fontsize=size)
+    ax[1].set_xlabel('Epoch', fontsize=size)
+    # ax[1].set_ylabel("Loss", fontsize=size)
+
+    ax2.set_ylabel("Acc", fontsize=size)
+
+    ax[1].grid(b=False, axis="y")
+    ax2.grid(b=False, axis="y")
+    ax[1].grid(b=True, axis="x")
+
+    Path = r"./无数据增强CIFAR10"
+    fig.savefig(Path + "/" + "无数据增强.png", bbox_inches='tight', pad_inches=0)
+    fig.show()
+    plt.style.use('default')
+
+
+full_Plot()
